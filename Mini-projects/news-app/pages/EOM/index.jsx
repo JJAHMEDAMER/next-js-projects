38844c8079e0bf2e999a757b69d8378e16a58266
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-11 09:57:52 +0200

    navbar in EOM page

--- Mini-projects/news-app/pages/EOM/index.jsx
+++ Mini-projects/news-app/pages/EOM/index.jsx
@@ -1,14 +1,20 @@
+// Comp
+import { Navbar } from "../../components/Navbar"
+
 // Styling
 import style from "./EOM.module.css"
 
 export default function EOM({ data }) {
     console.log(data) // Outputs in client and server
-    return <div className={style.container}>
-        <h1>Employee Of The Month</h1>
-        <h2>{data.name}</h2>
-        <h3>{data.position}</h3>
-        <img src={data.image} alt="" />
-        <h5>{data.description}</h5>
+    return <div>
+        <Navbar />
+        <div className={style.container}>
+            <h1>Employee Of The Month</h1>
+            <h2>{data.name}</h2>
+            <h3>{data.position}</h3>
+            <img src={data.image} alt="" />
+            <h5>{data.description}</h5>
+        </div>
     </div>
 }
 
