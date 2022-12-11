2392e6d48a13ccc069d118598cbc5ed3186ae26c
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-11 09:42:50 +0200

    navbar content style

---
+++ Mini-projects/news-app/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import styles from "./components.module.css"
+
+export const Navbar = () => {
+    return <div className={styles.navbar}>
+        <h1>Home</h1>
+        <h1>News</h1>
+        <h1>EOM</h1>
+        <h1>Twitter</h1>
+    </div>
+} 
