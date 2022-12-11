25340be79e0a91fef3434e69ce1c48ec516fcd2c
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-11 09:56:16 +0200

    next JS router

--- Mini-projects/news-app/components/Navbar.jsx
+++ Mini-projects/news-app/components/Navbar.jsx
@@ -1,10 +1,15 @@
+// Next JS 
+import { useRouter } from "next/router"
+
+// Styles
 import styles from "./components.module.css"
 
 export const Navbar = () => {
+    const router = useRouter();
     return <div className={styles.navbar}>
-        <h1>Home</h1>
+        <h1 onClick={() => router.push("/Home")}>Home</h1>
         <h1>News</h1>
-        <h1>EOM</h1>
-        <h1>Twitter</h1>
+        <h1 onClick={() => router.push("/EOM")}>EOM</h1>
+        <h1 onClick={() => window.location.href = "https://www.twitter.com"}>Twitter</h1>
     </div>
 } 
