b68560b89a1636fcecd88464603c5624e1a38c14
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-11 11:41:55 +0200

    paginator section in news

--- Mini-projects/news-app/pages/News/[pageNumber].jsx
+++ Mini-projects/news-app/pages/News/[pageNumber].jsx
@@ -1,8 +1,11 @@
 import { Navbar } from "../../components/Navbar";
 import styles from "./News.module.css"
 
+import { useRouter } from "next/router"
+
 export default function News({ data, pageNumber }) {
-    console.log(data)
+    // console.log(data)
+    const router = useRouter()
     return <div className={styles.container}>
         <Navbar />
         {data.articles.map((item, index) => {
@@ -11,11 +14,32 @@ export default function News({ data, pageNumber }) {
                 <h2>Author: {item.author}</h2>
                 <h3>Source: {item.source.name}</h3>
                 <img src={item.urlToImage} alt="" />
+                <h2>{item.description}</h2>
                 <p onClick={() => window.location.href = item.url}>Read More...</p>
                 <hr></hr>
             </div>)
         }
         )}
+
+        <div className={styles.paginator}>
+            <div className={pageNumber === 1 ? styles.nonactive : null}
+                onClick={() => {
+                    if (pageNumber > 1) {
+                        router.push(`/News/${pageNumber - 1}`)
+                    }
+                }}
+            >Prev. Page
+            </div>
+            <div className={styles.number}>{pageNumber}</div>
+            <div className={pageNumber === 5 ? styles.nonactive : null}
+                onClick={() => {
+                    if (pageNumber < 5) {
+                        router.push(`/News/${pageNumber + 1}`)
+                    }
+                }}
+            >Next Page
+            </div>
+        </div>
     </div>
 }
 
